2a5c994cdc07cc9b9a3fc65b41f5907e3221e143
areskib <areski.bacha@gmail.com>
2026-08-04 17:48:32 +0200

    fix: corrige le composant LWC orders (filtre par compte/statut, appel Apex, affichage conditionnel)

--- force-app/main/default/lwc/orders/orders.js
+++ force-app/main/default/lwc/orders/orders.js
@@ -1,16 +1,21 @@
-import { LightningElement, api } from 'lwc';
-// TODO - récupérer la méthode apex permettant de faire ce calcul
+import { LightningElement, api, wire } from 'lwc';
+import getSumOrdersByAccount from '@salesforce/apex/MyTeamOrdersController.getSumOrdersByAccount';
 
 export default class Orders extends LightningElement {
 
-    sumOrdersOfCurrentAccount;
     @api recordId;
+    sumOrdersOfCurrentAccount;
 
-    connectedCallback() {
-        this.fetchSumOrders();
+    @wire(getSumOrdersByAccount, { accountId: '$recordId' })
+    wiredSum({ data, error }) {
+        if (data !== undefined) {
+            this.sumOrdersOfCurrentAccount = data;
+        } else if (error) {
+            this.sumOrdersOfCurrentAccount = 0;
+        }
     }
 
-    fetchSumOrders() {
-        // TODO - récupérer le montant total des Orders sur le compte avec la méthode apex
+    get hasOrders() {
+        return this.sumOrdersOfCurrentAccount > 0;
     }
 }
